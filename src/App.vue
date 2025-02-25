adc02293a3d6764294c2cd575a855d1e5e56b3b5
luchamorro <chamorro.lucia@gmail.com>
2025-02-25 11:56:29 +0100

    carga de imagenes de la api

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <MarvelHome msg="Welcome to Your Vue.js App"/>
+ 
+  <TimelineMarvel />
+
+
 </template>
 
 <script setup>
-import MarvelHome from './components/MarvelHome.vue'
 
+import MarvelHome from './components/MarvelHome.vue';
+import TimelineMarvel from './components/TimelineMarvel.vue';
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
