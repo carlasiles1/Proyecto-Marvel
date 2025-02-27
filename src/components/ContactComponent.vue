89efd493644747858eb405d0a1664d6200d78ff1
carlasiles1 <carlasiles.19cs@gmail.com>
2025-02-27 10:31:20 +0100

    subida-prueba-contacto-29

--- src/components/ContactComponent.vue
+++ src/components/ContactComponent.vue
@@ -1,37 +1,52 @@
 <template>
-    <section class="Contact">
-      <form id="formulario">
-        <h1>Contact Us</h1>
-        <div class="form-group">
-          <label for="nombre">Name</label>
-          <input type="text" id="nombre" name="nombre" required />
-        </div>
+  <main>
+    <form id="formulario">
+      <div class="form-group">
+        <label for="nombre">Name:</label>
+        <input type="text" id="nombre" name="nombre" required />
+      </div>
 
-        <div class="form-group">
-          <label for="email">E-mail:</label>
-          <input type="email" id="email" name="email" required />
-        </div>
+      <div class="form-group">
+        <label for="email">E-mail:</label>
+        <input type="email" id="email" name="email" required />
+      </div>
 
-        <div class="form-group">
-          <label for="mensaje">Message:</label>
-          <textarea id="mensaje" name="mensaje" required></textarea>
-        </div>
-      </form> 
-    </section>
-  </template>
+      <div class="form-group">
+        <label for="mensaje">Message:</label>
+        <textarea id="mensaje" name="mensaje" required></textarea>
+      </div>
+      <div>
 
-  <style scoped>
+      </div>
+    </form> 
+  </main>
+</template>
 
-  *{
+<style>
+main{
+  background-color: #191129;
+  display: flex;
+  height: 100vh;    
+  justify-content: center;
+  align-items: center;
   margin: -0.5rem;
-  padding: 0rem;
-  background-color:black;
 }
-.main{
-    background-color:black;
-    height:100px;
-    width:100px;
+h1{
+color:white;
+}
+.form-group{
+display:flex;
+flex-direction:column;
+margin:20px;
+
+}
+label{
+color:white;
+}
+textarea{
+width:100%;
+height:100px;
 }
 
 
-  </style>
+</style>
